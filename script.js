17ea4d1856440045e8f0c274929c108327eee299
julianna-spec <wunmia454@gmail.com>
2025-05-30 12:29:23 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("learnMoreBtn").addEventListener("click", () => {
-  window.scrollTo({
-    top: document.getElementById("features").offsetTop,
-    behavior: "smooth"
-  });
-});
